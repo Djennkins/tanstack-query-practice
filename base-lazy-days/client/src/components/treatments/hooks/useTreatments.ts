10672d71d2cb3ec3c1ac9356daf90dfb8e3155e9
Djennkins <djennkins228@gmail.com>
2025-10-29 08:31:38 +0200

    55. Update Re-Fetch Options

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -6,24 +6,31 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const { data } = await axiosInstance.get("/treatments");
-  return data;
+    const { data } = await axiosInstance.get("/treatments");
+    return data;
 }
 
 export function useTreatments(): Treatment[] {
-  const fallback: Treatment[] = [];
+    const fallback: Treatment[] = [];
 
-  const { data = fallback } = useQuery({
-    queryKey: [queryKeys.treatments],
-    queryFn: getTreatments,
-  });
-  return data;
+    const { data = fallback } = useQuery({
+        queryKey: [queryKeys.treatments],
+        queryFn: getTreatments,
+        staleTime: 600000,
+        gcTime: 900000,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+    });
+    return data;
 }
 
 export function usePrefetchTreatments() {
-  const queryClient = useQueryClient();
-  queryClient.prefetchQuery({
-    queryKey: [queryKeys.treatments],
-    queryFn: getTreatments,
-  });
+    const queryClient = useQueryClient();
+    queryClient.prefetchQuery({
+        queryKey: [queryKeys.treatments],
+        queryFn: getTreatments,
+        staleTime: 600000,
+        gcTime: 900000,
+    });
 }
