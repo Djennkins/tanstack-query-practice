1c022133e6fb38a2faf77a391a4596853a351ada
Djennkins <djennkins228@gmail.com>
2025-11-05 06:56:28 +0200

    feat: 73. Writing Optimistic Update

--- base-lazy-days/client/src/components/user/UserProfile.tsx
+++ base-lazy-days/client/src/components/user/UserProfile.tsx
@@ -1,12 +1,12 @@
 import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Stack,
+    Box,
+    Button,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
 } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
 import { useEffect } from "react";
@@ -18,60 +18,74 @@ import { UserAppointments } from "./UserAppointments";
 
 import { useLoginData } from "@/auth/AuthContext";
 
+import { MUTATION_KEY } from "./hooks/usePatchUser";
+import { useMutationState } from "@tanstack/react-query";
+import { User } from "@shared/types";
+
 export function UserProfile() {
-  const { userId } = useLoginData();
-  const { user } = useUser();
-  const patchUser = usePatchUser();
-  const navigate = useNavigate();
+    const { userId } = useLoginData();
+    const { user } = useUser();
+    const patchUser = usePatchUser();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    // use login data for redirect, for base app that doesn't
-    //   retrieve user data from the server yet
-    if (!userId) {
-      navigate("/signin");
+    useEffect(() => {
+        // use login data for redirect, for base app that doesn't
+        //   retrieve user data from the server yet
+        if (!userId) {
+            navigate("/signin");
+        }
+    }, [userId, navigate]);
+
+    const pendingData = useMutationState({
+        filters: { mutationKey: [MUTATION_KEY], status: "pending" },
+        select: (mutation) => mutation.state.variables as User,
+    });
+
+    const pendingUser = pendingData ? pendingData[0]  : null;
+
+    const formElements = ["name", "address", "phone"];
+    interface FormValues {
+        name: string;
+        address: string;
+        phone: string;
     }
-  }, [userId, navigate]);
 
-  const formElements = ["name", "address", "phone"];
-  interface FormValues {
-    name: string;
-    address: string;
-    phone: string;
-  }
-
-  return (
-    <Flex minH="84vh" textAlign="center" justify="center">
-      <Stack spacing={8} mx="auto" w="xl" py={12} px={6}>
-        <UserAppointments />
-        <Stack textAlign="center">
-          <Heading>Your information {user?.name}</Heading>
-        </Stack>
-        <Box rounded="lg" bg="white" boxShadow="lg" p={8}>
-          <Formik
-            enableReinitialize
-            initialValues={{
-              name: user?.name ?? "",
-              address: user?.address ?? "",
-              phone: user?.phone ?? "",
-            }}
-            onSubmit={(values: FormValues) => {
-              patchUser({ ...user, ...values });
-            }}
-          >
-            <Form>
-              {formElements.map((element) => (
-                <FormControl key={element} id={element}>
-                  <FormLabel>{element}</FormLabel>
-                  <Field name={element} as={Input} />
-                </FormControl>
-              ))}
-              <Button mt={6} type="submit">
-                Update
-              </Button>
-            </Form>
-          </Formik>
-        </Box>
-      </Stack>
-    </Flex>
-  );
+    return (
+        <Flex minH="84vh" textAlign="center" justify="center">
+            <Stack spacing={8} mx="auto" w="xl" py={12} px={6}>
+                <UserAppointments />
+                <Stack textAlign="center">
+                    <Heading>
+                        Your information{" "}
+                        {pendingUser ? pendingUser.name : user?.name}
+                    </Heading>
+                </Stack>
+                <Box rounded="lg" bg="white" boxShadow="lg" p={8}>
+                    <Formik
+                        enableReinitialize
+                        initialValues={{
+                            name: user?.name ?? "",
+                            address: user?.address ?? "",
+                            phone: user?.phone ?? "",
+                        }}
+                        onSubmit={(values: FormValues) => {
+                            patchUser({ ...user, ...values });
+                        }}
+                    >
+                        <Form>
+                            {formElements.map((element) => (
+                                <FormControl key={element} id={element}>
+                                    <FormLabel>{element}</FormLabel>
+                                    <Field name={element} as={Input} />
+                                </FormControl>
+                            ))}
+                            <Button mt={6} type="submit">
+                                Update
+                            </Button>
+                        </Form>
+                    </Formik>
+                </Box>
+            </Stack>
+        </Flex>
+    );
 }
--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,11 +1,14 @@
 import jsonpatch from "fast-json-patch";
 
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { User } from "@shared/types";
 
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 import { useUser } from "./useUser";
 import { toast } from "@/components/app/toast";
+import { queryKeys } from "@/react-query/constants";
+
+export const MUTATION_KEY = "patch-user";
 
 // for when we need a server function
 async function patchUserOnServer(
@@ -28,16 +31,25 @@ async function patchUserOnServer(
 }
 
 export function usePatchUser() {
-    const { user, updateUser } = useUser();
+    const queryClient = useQueryClient();
+
+    const { user } = useUser();
 
     const { mutate: patchUser } = useMutation({
+        mutationKey: [MUTATION_KEY],
         mutationFn: (newData: User) => patchUserOnServer(newData, user),
-        onSuccess: (data: User | null) => {
+        onSuccess: () => {
             toast({
                 title: "User updated",
                 status: "success",
             });
-            updateUser(data);
+            // updateUser(data);
+        },
+        onSettled: () => {
+            // return promise to maintain 'inProgress' status until the query invalidation is complete
+            return queryClient.invalidateQueries({
+                queryKey: [queryKeys.user],
+            });
         },
     });
 
