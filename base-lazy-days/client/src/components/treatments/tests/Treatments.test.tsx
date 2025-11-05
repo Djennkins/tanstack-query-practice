567e8e99b4367b593c564425732e79880cb14143
Djennkins <djennkins228@gmail.com>
2025-11-05 07:32:33 +0200

    feat: 78. Testing Rendered Query Data

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,13 @@
-import { render } from "@/test-utils";
+import { render, screen } from "@/test-utils";
 
 import { Treatments } from "../Treatments";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
     render(<Treatments />);
+
+    const treatmentTitles = await screen.findAllByRole("heading", {
+        name: /massage|facial|scrub/i,
+    });
+
+    expect(treatmentTitles).toHaveLength(3);
 });
