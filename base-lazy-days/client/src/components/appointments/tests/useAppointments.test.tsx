6a30b5a503303879edb48384dcd63377152362c8
Djennkins <djennkins228@gmail.com>
2025-11-06 09:15:03 +0200

    feat: 83. Test Appointments Filter

--- base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,10 +1,43 @@
-// import { act, renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 
 import { useAppointments } from "../hooks/useAppointments";
 import { AppointmentDateMap } from "../types";
 
-// import { createQueryClientWrapper } from "@/test-utils";
+import { createQueryClientWrapper } from "@/test-utils";
+
+// a helper function to get the total number of appoinments from an AppointmentDateMap
+function getAppointmentCount(appointments: AppointmentDateMap) {
+    return Object.values(appointments).reduce(
+        (runningCount, appoinmentOnDate) =>
+            runningCount + appoinmentOnDate.length,
+        0
+    );
+}
 
 test("filter appointments by availability", async () => {
-  // test goes here
+    const { result } = renderHook(() => useAppointments(), {
+        wrapper: createQueryClientWrapper(),
+    });
+
+    // wait for appoinments to populate
+    await waitFor(() =>
+        expect(
+            getAppointmentCount(result.current.appointments)
+        ).toBeGreaterThan(0)
+    );
+
+    // appoinments start out filtered (show only available appointments)
+    const filteredAppoinmentsLength = getAppointmentCount(
+        result.current.appointments
+    );
+
+    // set to return all appointments
+    act(() => result.current.setShowAll(true));
+
+    //wait for count of appoinments to be greater than when filtered
+    await waitFor(() =>
+        expect(
+            getAppointmentCount(result.current.appointments)
+        ).toBeGreaterThan(filteredAppoinmentsLength)
+    );
 });
